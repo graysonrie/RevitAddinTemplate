1cabf949409cb51eaaffe2b86e2b933c92a7bf3b
graysonrie <graysonr12@icloud.com>
2025-05-21 14:03:18 -0500

    added next.js

---
+++ nextwpf/next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  assetPrefix: "./", // Use relative path
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.output = config.output || {};
+      config.output.publicPath = "./_next/"; 
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
